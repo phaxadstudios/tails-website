02b1c3158e4b2499e706abd9efb7883c11a97cd3
Nnamchi Israel <91748627+phaxadstudios@users.noreply.github.com>
2022-01-01 17:51:36 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
-<?php
-  include 'paritals/construct.phaxad.php';
-?>
+
 <!DOCTYPE html>
 <html :class="{ 'theme-dark': dark }" x-data="data()" lang="en">
   <head>
@@ -982,3 +980,5 @@
         </p>
     </div>
 </section>
+  </body>
+</html>
